bc0a74bd44f8e735ce52dadab1b9a5b6f2d79545
janaalsalem <janaalsalem@outlook.sa>
2024-07-04 13:45:37 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,13 @@ void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      //
       home:  SplashScreen(),
     );
   }
