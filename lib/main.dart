a10bccfbc280f12d0f3ad381b08dbacf980dee6e
janaalsalem <janaalsalem@outlook.sa>
2024-07-19 19:42:52 +0300

    - Fix Launching error in mac

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mackenzie_academy/auth/auth.dart';
 
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    // options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
